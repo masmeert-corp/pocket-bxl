8bfd710cd665b12898ab001c63f020e0561d915c
Massimo Meert <pro@masmeert.com>
2026-05-29 17:43:12 +0200

    add expo-font

--- apps/native/app/_layout.tsx
+++ apps/native/app/_layout.tsx
@@ -1,6 +1,14 @@
 import "@/global.css";
+import {
+  HankenGrotesk_400Regular,
+  HankenGrotesk_500Medium,
+  HankenGrotesk_600SemiBold,
+  HankenGrotesk_700Bold,
+  useFonts,
+} from "@expo-google-fonts/hanken-grotesk";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
 import { HeroUINativeProvider } from "heroui-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { KeyboardProvider } from "react-native-keyboard-controller";
@@ -8,7 +16,10 @@ import { KeyboardProvider } from "react-native-keyboard-controller";
 import { AppThemeProvider } from "@/contexts/app-theme-context";
 import { queryClient } from "@/utils/trpc";
 
+SplashScreen.preventAutoHideAsync();
+
 function StackLayout() {
+  SplashScreen.hideAsync();
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
@@ -17,6 +28,15 @@ function StackLayout() {
 }
 
 export default function Layout() {
+  const [loaded] = useFonts({
+    HankenGrotesk_400Regular,
+    HankenGrotesk_500Medium,
+    HankenGrotesk_600SemiBold,
+    HankenGrotesk_700Bold,
+  });
+
+  if (!loaded) return null;
+
   return (
     <QueryClientProvider client={queryClient}>
       <GestureHandlerRootView style={{ flex: 1 }}>
