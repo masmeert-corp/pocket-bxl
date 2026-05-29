ad6c1c48d57a565cc8eedd46019913f246ea2d7d
Massimo Meert <massimo@apkiosk.com>
2026-05-29 13:21:38 +0200

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,34 @@
+import { expo } from "@better-auth/expo";
+import { createDb } from "@pocket-bxl/db";
+import * as schema from "@pocket-bxl/db/schema/auth";
+import { env } from "@pocket-bxl/env/server";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+
+export function createAuth() {
+  const db = createDb();
+
+  return betterAuth({
+    database: drizzleAdapter(db, {
+      provider: "pg",
+
+      schema: schema,
+    }),
+    trustedOrigins: [env.CORS_ORIGIN, "pocket-bxl://", "exp://", "http://localhost:8081"],
+    emailAndPassword: {
+      enabled: true,
+    },
+    secret: env.BETTER_AUTH_SECRET,
+    baseURL: env.BETTER_AUTH_URL,
+    advanced: {
+      defaultCookieAttributes: {
+        sameSite: "none",
+        secure: true,
+        httpOnly: true,
+      },
+    },
+    plugins: [expo()],
+  });
+}
+
+export const auth = createAuth();
